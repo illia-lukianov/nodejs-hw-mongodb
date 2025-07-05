64888d01cb8b1aea8290c932a01fffdec25ecd71
Lukianov Illia <Lukjanovilly@gmail.com>
2025-07-05 21:05:19 +0300

    add nev script to run production

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ export default function setupServer() {
   app.use(cors());
   app.use(pinoHttp());
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-  app.get('/', fetchContacts)
+  app.get('/contacts', fetchContacts)
   app.get('/contacts/:contactId', fetchContactById)
   app.use((req, res, next) => {
   res.status(404).json({status:404, message: 'Not found' });
